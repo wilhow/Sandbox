7fb0d9374c39005516783a5524a1ebd823982f4d
wilhow <wil.how@gmail.com>
2015-03-06 10:27:54 -0600

    update the navs to work with 2 impulsejs dropdown and fix the styling. This should be the new base.

--- js/navs/page.js
+++ js/navs/page.js
@@ -1,35 +1,70 @@
-var menuEl = document.querySelector('.pull-down-menu')
-console.log(menuEl)
-var handleEls = document.querySelectorAll('.nav-header, .close-handle')
 var isOpen = false
-var boundry = new Impulse.Boundry({ top: 0, bottom: window.innerHeight, left: 0, right: 0 })
+//vertical menu
+var vMenuEl = document.querySelector('.vpull-down-menu'); //The menu object
+var vHandleEls = document.querySelectorAll('.vert-header, .vclose-handle'); //The collection of menu object. This also triggers the menu to slide down.
+var vBoundry = new Impulse.Boundary({ top: 0, bottom: window.innerHeight, left: 0, right: 0 }) //CSS boundary of the menu. Thus it's the actual div that's being slid.
+var vMenu = new Impulse(vMenuEl)
+  .style('translateY', function(x, y) { return y + 'px' }); //The object representation of the menu
 
-var menu = new Impulse(menuEl)
-  .style('translateY', function(x, y) { return y + 'px' })
+var vDrag = vMenu.drag({ handle: vHandleEls, boundary: vBoundry, direction: 'vertical' }); //The physics of the menu object
 
-var drag = menu.drag({ handle: handleEls, boundry: boundry, direction: 'vertical' })
-
-function end() {
-  if(this.moved()) {
-    isOpen = menu.direction('down')
+  function vend() {
+	  console.log("vert:end:")
+    if(this.moved()) {
+      isOpen = vMenu.direction('down')
   } else {
     isOpen = !isOpen
     if(isOpen) {
-      menu.velocity(0, 2000)
+      vMenu.velocity(0, 2000)
     }
   }
 
   if(isOpen) {
-    menuEl.classList.add('open')
-    menu.accelerate({ acceleration: 1500, bounceAcceleration: 4000, bounce: this.moved() })
-      .to(0, boundry.bottom).start()
+    vMenuEl.classList.add('open')
+	vMenu.accelerate({ acceleration: 1500, bounceAcceleration: 4000, bounce: true })
+      .to(0, vBoundry.bottom).start()
       .then(function() {
       })
   } else {
-    menuEl.classList.remove('open')
-    menu.spring({ tension: 100, damping: 15 })
-      .to(0, boundry.top).start()
+    vMenuEl.classList.remove('open')
+    vMenu.spring({ tension: 100, damping: 15 })
+      .to(0, vBoundry.top).start()
   }
 }
 
-drag.on('end', end)
+vDrag.on('end', vend)
+
+//Horizontal menu
+var hMenuEl = document.querySelector('.pull-down-menu'); //The menu object
+var hHandleEls = document.querySelectorAll('.horz-header, .hclose-handle'); //The collection of menu object. This also triggers the menu to slide down.
+var hBoundry = new Impulse.Boundary({ top: 0, bottom: window.innerHeight, left: 0, right: 0 }) //CSS boundary of the menu. Thus it's the actual div that's being slid.
+var hMenu = new Impulse(hMenuEl)
+  .style('translateY', function(x, y) { return y + 'px' }); //The object representation of the menu
+
+var hDrag = hMenu.drag({ handle: hHandleEls, boundary: hBoundry, direction: 'vertical' }); //The physics of the menu object
+
+  function hend() {
+	  console.log("horz:end:")
+    if(this.moved()) {
+      isOpen = hMenu.direction('down')
+  } else {
+    isOpen = !isOpen
+    if(isOpen) {
+      hMenu.velocity(0, 2000)
+    }
+  }
+
+  if(isOpen) {
+    hMenuEl.classList.add('open')
+	hMenu.accelerate({ acceleration: 1500, bounceAcceleration: 4000, bounce: true })
+      .to(0, hBoundry.bottom).start()
+      .then(function() {
+      })
+  } else {
+    hMenuEl.classList.remove('open')
+    hMenu.spring({ tension: 100, damping: 15 })
+      .to(0, hBoundry.top).start()
+  }
+}
+
+hDrag.on('end', hend)
